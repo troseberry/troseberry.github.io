703d7770f816a70039c6281f960f4a7a664489e8
troseberry <tayroseberry@gmail.com>
2019-06-08 19:08:36 -0500

    Initial Commit of files

---
+++ js/voyage-portfolio.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+function VoyageScriptingButtonClick() {
+	
+	if (document.getElementById("scripting-button").className == 'default-btn'){
+		document.getElementById("voyage-scripting").style.display = 'block';
+		document.getElementById("voyage-leveldesign").style.display = 'none';
+	
+		document.getElementById("scripting-button").className = 'default-btn-active';
+		document.getElementById("ld-button").className = 'default-btn';
+	}
+}
+
+function VoyageLevelButtonClick() {
+	
+	if (document.getElementById("ld-button").className == 'default-btn'){
+	
+		document.getElementById("voyage-scripting").style.display = 'none';
+		document.getElementById("voyage-leveldesign").style.display = 'block';
+
+		document.getElementById("scripting-button").className = 'default-btn';
+		document.getElementById("ld-button").className = 'default-btn-active';
+	}
+	
+}
